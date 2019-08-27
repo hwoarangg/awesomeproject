91e65c57eee3790f8f6260e7fbfde5b80256e3a0
hwoarangg <ersan.gnd@yandex.com.tr>
2019-08-27 23:17:18 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace awesomeproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int a = "sdf";
+        }
+    }
+}
